2d67b596ba2aec3bcbae7c5b05b81d9acf423bda
Rohan <rshanbh@uwo.ca>
2019-04-11 03:47:37 -0400

    Header complete

--- index.js
+++ index.js
@@ -4,13 +4,14 @@
 
 // Import a library to create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 
 // Create a componenent
 const App = () => {
   return (
-    <Text>Some Text</Text>
+    <Header headerText={'Albums'} />
   );
 };
 
