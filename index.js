8a7b97873b46b5029cd1ae3f6b0aefcaac60e8ce
Rohan <rshanbh@uwo.ca>
2019-04-11 04:40:00 -0400

    Album list fetched from database

--- index.js
+++ index.js
@@ -4,14 +4,18 @@
 
 // Import a library to create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList'
 
 
 // Create a componenent
 const App = () => {
   return (
-    <Header headerText={'Albums'} />
+    <View>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
   );
 };
 
